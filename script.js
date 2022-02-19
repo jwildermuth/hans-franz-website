a8b9593ecc24adf6977c400b13f34697040fe616
jw laptop <jmw33322@hotmail.com>
2022-02-19 07:11:50 -0500

    init commit: moved this site from another repo in order to use GH Pages

---
+++ script.js
@@ -0,0 +1,28 @@
+'use-strict';
+
+
+const navButton = document.querySelector('#nav-button');
+const mobileNav = document.querySelector('.mobile-nav-wrapper');
+const mobileNavUl = document.querySelector('.mobile-nav-list');
+
+
+// hamburger button toggle
+navButton.addEventListener('click', () => {
+  mobileNav.classList.toggle('display-block');
+})
+
+
+// hide mobile nav when clicking outside of it
+window.addEventListener('mouseup', function(e) {
+  if (e.target != navButton &&
+      e.target != mobileNav &&
+      e.target != mobileNavUl) {
+    mobileNav.classList.remove('display-block');
+  }
+})
+
+
+
+
+
+
